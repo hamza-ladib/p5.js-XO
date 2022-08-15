226c200e63e13e5c0b650f682a9771815bffc4ee
hamza-ladib <hamza.ladib@edu.uiz.ac.ma>
2022-08-15 02:14:34 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+///// main fonction////////
+
+
+
+
+
+//////////////////////// variables //
+let canvas;
+function setup(){
+ canvas=createCanvas(400,400);
+ canvas.id("cnv");
+}
+function draw(){
+    background("red");
+
+}
